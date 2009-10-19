177938e9c0a0136dcb17c250fd8b160d0782e882
troelsk <troelsk@781c6ece-3a97-4421-8ea0-c2586a129dfe>
2009-10-19 11:08:15 +0000

    Added wx29.cpp

---
+++ wx/src/wx29.cpp
@@ -0,0 +1,16 @@
+// wx29.cpp
+// Copyright (c) 2009 by Troels K. All rights reserved.
+// License: wxWindows
+
+#include "precomp.h"
+#include "wx29.h"
+
+BEGIN_EVENT_TABLE(wxDocManagerEx, wxDocManager)
+END_EVENT_TABLE()
+
+#if (wxVERSION_NUMBER < 2900)
+void wxDocManagerEx::OnUpdateDisableIfNoDoc(wxUpdateUIEvent& event)
+{
+    event.Enable( GetCurrentDocument() != NULL );
+}
+#endif
