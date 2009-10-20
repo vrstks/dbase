d7cfa99bdd34087789deedfecf3212c7e8a5e55e
troelsk <troelsk@781c6ece-3a97-4421-8ea0-c2586a129dfe>
2009-10-20 20:07:18 +0000

    Using wxprec.h instead of wx.h

--- wx/src/precomp.h
+++ wx/src/precomp.h
@@ -2,14 +2,16 @@
 // Copyright (c) 2009 by Troels K. All rights reserved.
 // License: wxWindows Library Licence, Version 3.1 - see LICENSE.txt
 
-#include <wx/wx.h>
-#include <wx/mdi.h>
-#include <wx/docview.h>
-#include <wx/docmdi.h>
-#include <wx/listctrl.h>
-#include <wx/xrc/xmlres.h>
-#include <wx/fs_zip.h>
-#include <wx/config.h>
-#include <wx/clipbrd.h>
-#include <wx/dataview.h>
-#include <wx/stockitem.h>
+#define WX_PRECOMP
+#include "wx/wxprec.h"
+
+#include "wx/mdi.h"
+#include "wx/docview.h"
+#include "wx/docmdi.h"
+#include "wx/listctrl.h"
+#include "wx/xrc/xmlres.h"
+#include "wx/fs_zip.h"
+#include "wx/config.h"
+#include "wx/clipbrd.h"
+#include "wx/dataview.h"
+#include "wx/stockitem.h"
