a9e536159b1ccc5c7b4539b10be6d2acb19e155a
troelsk <troelsk@781c6ece-3a97-4421-8ea0-c2586a129dfe>
2010-01-31 18:45:26 +0000

    Added DataWindow.xaml

--- cs/wpf/App.xaml.cs
+++ cs/wpf/App.xaml.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Data;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 
 namespace wpf
 {
@@ -13,60 +9,4 @@ namespace wpf
 	public partial class App : System.Windows.Application
 	{
 	}
-
-   class Program : Window
-   {
-      [STAThread]
-      static void Main(string[] args)
-      {
-         TableWindow tablewindow = new TableWindow();
-         new Application().Run(tablewindow);
-      }
-   }
-
-   class MyDataTable : DataTable
-   {
-      public MyDataTable(DBase.Recordset recordset) : base(recordset.TableName)
-      {
-         foreach (DBase.FieldInfo field in recordset.Fields)
-         {
-            var column = new DataColumn(field.Name);
-            column.DataType = field.GetType();
-            Columns.Add(column);
-         }
-      }
-   }
-   public class TableWindow : Window
-   {
-      public TableWindow()
-      {
-         //Create DataTable and put some data in it
-         string filename = @"h:\cpcload.dbf";
-         var recordset = new DBase.Recordset();
-         recordset.Open(filename, System.IO.FileMode.Open);
-         var table = new MyDataTable(recordset);
-         
-         var listview = new ListView();
-         var gridview = new GridView();
-
-         foreach (DBase.FieldInfo field in recordset.Fields)
-         {
-            var gvcolumn = new GridViewColumn();
-            gvcolumn.Header = field.Name;
-            gvcolumn.DisplayMemberBinding = new Binding(field.Name);
-            gridview.Columns.Add(gvcolumn);
-         }
-
-         listview.View = gridview;
-
-         var bind = new Binding();
-         bind.Source = recordset;
-
-         //listview.ItemsSource = recordset;
-         listview.DataContext = table;
-         listview.SetBinding(ListView.ItemsSourceProperty, bind);
-
-         Content = listview;
-      }
-   }
 }
---
+++ cs/wpf/DataWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace wpf
+{
+   /// <summary>
+   /// Interaction logic for DataWindow.xaml
+   /// </summary>
+   public partial class DataWindow : Window
+   {
+      public DataWindow()
+      {
+         InitializeComponent();
+         
+         string filename = @"h:\cpcload.dbf";
+         var recordset = new DBase.Recordset();
+         recordset.Open(filename, System.IO.FileMode.Open);
+         var table = new MyDataTable(recordset);
+         Title = string.Format("{0} [{1}] - dbf library", System.IO.Path.GetDirectoryName(recordset.Filename), table.TableName);
+
+         var gridview = new GridView();
+
+         foreach (DBase.FieldInfo field in recordset.Fields)
+         {
+            var gvcolumn = new GridViewColumn();
+            gvcolumn.Header = field.Name;
+            gvcolumn.DisplayMemberBinding = new Binding(field.Name);
+            gridview.Columns.Add(gvcolumn);
+         }
+
+         list.View = gridview;
+
+         var bind = new Binding();
+         bind.Source = recordset;
+
+         list.ItemsSource = recordset;
+         //list.DataContext = new ObservableCollection<DBase.Recordset>(recordset);
+         list.SetBinding(ListView.ItemsSourceProperty, bind);
+      }
+   }
+
+   class MyDataTable : DataTable
+   {
+      public MyDataTable(DBase.Recordset recordset) : base(recordset.TableName)
+      {
+         foreach (DBase.FieldInfo field in recordset.Fields)
+         {
+            var column = new DataColumn(field.Name);
+            column.DataType = field.GetType();
+            Columns.Add(column);
+         }
+      }
+   }
+}
